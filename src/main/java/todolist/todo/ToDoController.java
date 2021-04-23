22d8ae991a332985ff9980916a0bce5f3b3e7cc5
osman-developer <57558743+osman-developer@users.noreply.github.com>
2021-04-23 23:46:41 +0300

    second commit

--- src/main/java/todolist/todo/ToDoController.java
+++ src/main/java/todolist/todo/ToDoController.java
@@ -2,15 +2,19 @@ package todolist.todo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ToDoController {
 
+	@Autowired //used for dependecy injection
+	private ToDoService toDoService;
+	
 	@RequestMapping("/todos")
 	public List<ToDo> getAllToDos(){
-		return null;
+		return toDoService.getAllTodos();
 		
 	}
 }
---
+++ src/main/java/todolist/todo/ToDoService.java
@@ -0,0 +1,21 @@
+package todolist.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Service
+public class ToDoService {
+	private List<ToDo> todos = new ArrayList<>(Arrays.asList(
+			new ToDo(1, "task1", "summary1", "descript1"),
+			new ToDo(2, "task2", "summary2", "descript2")
+			)
+		);
+	
+	public List<ToDo> getAllTodos(){
+		return todos;
+	}
+}
